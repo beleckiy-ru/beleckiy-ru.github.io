29391c725d8a29b5e22ca9b66e21db433bbe8ab3
Roman Akinfold <akinfold@gmail.com>
2019-01-29 01:46:18 +0300

    remove source maps and old map

--- js/script.js
+++ js/script.js
@@ -1,40 +1,5 @@
 'use strict';
 
-var map = void 0;
-var center = { lat: 56.018816, lng: 92.824230 };
-
-function initMap() {
-    var zoom = 18;
-    if (window.innerWidth < 768) {
-        zoom = 16;
-    }
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: center.lat + 0.0002, lng: center.lng },
-        zoom: zoom,
-        disableDefaultUI: true,
-        clickableIcons: false
-    });
-
-    google.maps.event.addListenerOnce(map, 'idle', function () {
-        setTimeout(function () {
-            var iw = $('.gm-style-iw').parent();
-            iw.addClass('custom-iw');
-            iw.find('> div:not(.gm-style-iw)').remove();
-        }, 1000);
-    });
-
-    var marker = new google.maps.Marker({
-        position: center,
-        map: map,
-        title: '660018, г. Красноярск, ул.Яковлева, д. 1а'
-    });
-
-    var infoWindow = new google.maps.InfoWindow({
-        content: '<span class="iw-content">660018, г. Красноярск, ул.Яковлева, д. 1а</span>'
-    });
-    infoWindow.open(map, marker);
-}
-
 function detectIE() {
     return navigator.userAgent.indexOf('MSIE') !== -1 || navigator.userAgent.toLowerCase().indexOf('firefox') !== -1 || navigator.appVersion.indexOf('Trident/') > 0 || navigator.appVersion.indexOf('Edge/') > 0;
 }
@@ -84,4 +49,3 @@ $(function () {
         }
     });
 });
-//# sourceMappingURL=script.js.map
