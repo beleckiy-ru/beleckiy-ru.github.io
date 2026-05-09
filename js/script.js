3edac4ac8254356227e9abe82ca73a00298b6084
Roman Akinfeev <akinfold@gmail.com>
2026-05-10 02:59:03 +0300

    update index.html to include service URLs, enhance sitemap.xml with new service pages, and modify deployment workflows to include service HTML files

--- js/script.js
+++ js/script.js
@@ -1,51 +1,10 @@
 'use strict';
 
-function detectIE() {
-    return navigator.userAgent.indexOf('MSIE') !== -1 || navigator.userAgent.toLowerCase().indexOf('firefox') !== -1 || navigator.appVersion.indexOf('Trident/') > 0 || navigator.appVersion.indexOf('Edge/') > 0;
-}
-
-function isMobile() {
-    return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-    );
-}
-
-function detectMobileFF() {
-    return navigator.userAgent.toLowerCase().indexOf('firefox') !== -1 && isMobile();
-}
-
-function slideTo(e, id) {
-    e.preventDefault();
-
-    var timeout = isMobile() ? 300 : 100;
-
-    setTimeout(function () {
-        $('#serviceModal').modal('show');
-        slide = id;
-
-        if (carousel) $('.slider').slick('slickGoTo', slide, false);
-    }, timeout);
-}
-
-var carousel = null;
-var slide = 0;
-
-$(function () {
-    if (detectIE() || detectMobileFF()) {
-        $('body').addClass('ie-detect');
+(function () {
+    var u = navigator.userAgent;
+    var ie = u.indexOf('MSIE') !== -1 || u.toLowerCase().indexOf('firefox') !== -1 || navigator.appVersion.indexOf('Trident/') > 0 || navigator.appVersion.indexOf('Edge/') > 0;
+    var m = u.toLowerCase().indexOf('firefox') !== -1 && /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(u);
+    if (ie || m) {
+        document.body.classList.add('ie-detect');
     }
-
-    $('#serviceModal').on('shown.bs.modal', function () {
-        if (!carousel) {
-            carousel = $('.slider').slick({
-                infinite: true,
-                initialSlide: slide,
-                slidesToShow: 1,
-                appendDots: '.slider__dots',
-                dots: true,
-                prevArrow: '.slider__arrow--prev',
-                nextArrow: '.slider__arrow--next',
-                arrows: true
-            });
-        }
-    });
-});
+})();
